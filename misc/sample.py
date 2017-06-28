92f5b04e59a67446344922fcb153eab9a54b2f22
t9md <taqumd@gmail.com>
2017-06-28 16:52:35 +0900

    better sample to check behavior

--- misc/sample.py
+++ misc/sample.py
@@ -2,6 +2,12 @@
 #
 # for index, letter in enumerate(string):
 #     print((letter, index))
+import sys
+import time
 
-print([p for p in
-    [0,1,2,3]])
+for i in range(10):
+    sys.stdout.write(str(i))
+    time.sleep(1)
+    sys.stdout.write("\r")
+# print([p for p in
+#     [0,1,2,3]])
